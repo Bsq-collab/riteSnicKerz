5f8d78bf254c719a78aadd1a4b6f43c522d1ee74
bleung329 <bleung@stuy.edu>
2018-05-23 15:01:45 -0400

    in memory for my fallen comrades, eliminated by the AP Chem and AP Physics C exams of the Board of College

--- testingRoom/dataTest/selfDataTest.py
+++ testingRoom/dataTest/selfDataTest.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Students(db.Model):
+class students(db.Model):
 	id = db.Column('student_id',db.Integer,primary_key=True)
 	osis = db.Column(db.Integer)
 	fname = db.Column(db.String(20))
@@ -23,10 +23,18 @@ class Students(db.Model):
 		self.fname = fname
 		self.lname = lname
 
+class teachers(db.Model):
+ 	id = db.Column('student_id',db.Integer,primary_key=True)
+	teacherID = db.Column(db.Integer)
+
+	def __init__(self,ood):
+		self.teacherID = ood 
 
 if __name__ == '__main__':
 	db.create_all()
 	newstudent = Students(207268863,'Brian','Leung')
+	newteacher = teachers(202020)
+	db.session.add(newteacher)
 	db.session.add(newstudent)
-	#db.session.commit()
+	db.session.commit()
 	app.run(debug = True)
