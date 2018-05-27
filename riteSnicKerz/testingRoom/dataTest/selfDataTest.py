8f95173d309724fe7023281a04b2f9f97a5d6d8b
bleung329 <bleung@stuy.edu>
2018-05-26 21:30:05 -0400

    oops, signing off

--- riteSnicKerz/testingRoom/dataTest/selfDataTest.py
+++ riteSnicKerz/testingRoom/dataTest/selfDataTest.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-import dataAccess2 as zoop
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.sqlite3'
@@ -14,10 +14,10 @@ class students(db.Model):
 	fname = db.Column(db.String(20))
 	lname = db.Column(db.String(20))
 	email = db.Column(db.String(30))
-	SCHEDULE = db.Column(db.String(1000))
+	SCHEDULE = db.Column(db.String(200))
 	O_average = db.Column(db.Float)
-	S_average = db.Column(db.String(200))
-	pw = db.Column(db.String(100))
+	S_average = db.Column(db.String(100))
+	pw = db.Column(db.String(20))
 
 	def __init__(self, osis, fname, lname):
 		self.osis = osis
@@ -40,19 +40,19 @@ class teachers(db.Model):
 	def __init__(self,ood):
 		self.teacherID = ood 
 
+def StoL(listring):
+	return json.loads(listring)
+
 if __name__ == '__main__':
 	db.create_all()
 	
-	newstudent = students(207268863,'Brian','Leung')
+	newstudent = students(1234,'Brian','Leung')
 	db.session.add(newstudent)
-	
-	newstudent = students(31415,"J","DW")
-	db.session.add(newstudent)
-	
+		
 	newteacher = teachers(202020)
 	db.session.add(newteacher)
 	
-	newstudent = newstudent.getStudent(207268863)
+	newstudent = newstudent.getStudent(1234)
 	print newstudent
 	
 	print "DONE"
