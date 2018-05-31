3065cc4dd05a42e4533d7647feac31c12c306c1d
bleung329 <bleung@stuy.edu>
2018-05-31 13:09:16 -0400

    e

--- riteSnicKerz/testingRoom/dataTest/selfDataTest.py
+++ riteSnicKerz/testingRoom/dataTest/selfDataTest.py
@@ -2,6 +2,8 @@ from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+bigList = " 'Spanish 1', 'Spanish 2', 'Spanish 3', 'Conversational Spanish', 'AP Spanish', 'Spanish Film', 'Mandarin 1', 'Mandarin 2', 'Mandarin 3', 'Conversational Mandarin', 'AP Mandarin', 'French 1', 'French 2', 'French 3', 'AP French', 'Japanese 1', 'Japanese 2', 'Japanese 3', 'AP Japanese', 'Latin 1', 'Latin 2', 'Latin 3', 'AP Latin', 'German 1', 'German 2', 'German 3', 'Italian 1', 'Italian 2', 'Italian 3', 'Algebra', 'Geometry', 'Euclidean Geometry & Algebra Enhanced', 'Algebra 2 / Trigonometry', 'Advanced Algebra with Precalculus', 'Precalculus', 'AP AB Calculus', 'AP BC Calculus', 'Calculus Applications', 'Multivariate Calculus', 'Freshman Math Team', 'AP Statistics', 'Global History Year 1', 'Advanced Topics Global History', 'AP Human Geography', 'AP European History', 'AP World History', 'Global History Year 2', 'US History', 'AP US History', 'Government', 'Economics', 'AP Government', 'AP Macroeconomics', 'AP Microeconomics', 'Introduction to CS 1st Semester', 'Introduction to CS 2nd Semester', 'AP Computer Science', 'Systems Level Programming', 'Graphics', 'Software Development', 'Technical Drawing', 'Music Appreciation', 'Art Appreciation', 'AP Music Theory', 'Health', 'English', 'Modern Biology', 'Topics in Biology', 'Human Diseases', 'Genetics', 'AP Biology', 'AP Pyschology', 'Chemistry', 'AP Chemistry', 'Organic Chemistry', 'Advanced Chemistry Lab', 'Physics', 'AP Physics B', 'AP Physics C',"
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -48,7 +50,9 @@ class classes(db.Model):
 		temp[str(num)] = {"teacher":techer,"room":rom,"roster":roost}
 		print temp
 		self.sections = json.dumps(temp)
-
+	#relationship()
+	#foreign key()
+	
 	def add_student(self,sect,osiss):
 		temp = json.loads(self.sections)
 		temp[str(sect)]["roster"].append(osiss)
