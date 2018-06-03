d8159096320ed5d2e9e8e43e0bf0c66a8a478b9e
bleung329 <bleung@stuy.edu>
2018-06-02 23:47:44 -0400

    hey, here in my garage, with my lamborghini

--- riteSnicKerz/testingRoom/ORMTest/ormTest.py
+++ riteSnicKerz/testingRoom/ORMTest/ormTest.py
@@ -19,9 +19,9 @@ class User(db.Model):
 	name = db.Column(db.String(20))
 	subscriptions = db.relationship('Channel',secondary=subs,backref=db.backref('subscribers',lazy = 'dynamic'))
 	#Note on secondary
-	#Tells helper table - hey use this table for things and yeah
+	#Tells helper table - hey use this table for connecting to the Channel so you can do stuff to it. 
 	#Note on backref
-	#Its basically saying: Hey Channel youre gonna have another variable and its gonna be called subscribers
+	#Its basically saying: Hey Channel youre gonna have another variable and its gonna be called subscribers. It'll be a list of subscribers
 
 class Channel(db.Model):
 	channel_id = db.Column(db.Integer, primary_key = True)
@@ -30,3 +30,30 @@ class Channel(db.Model):
 #example command:
 
 #$ *channel*.subscribers.append(*user*)
+
+if __name__ == '__main__':
+	db.create_all()
+	user1=User(name="bob")
+	if user1.query.filter_by(name="bob").first() is not None:
+		os.system("rm school.sqlite3")
+
+	user2=User(name="jay")
+	channel1=Channel(channel_name="Vanoss")
+	channel2=Channel(channel_name="SlowMoGuys")
+	channel1.subscribers.append(user1)
+	print "CHANNEL 1 SUBSCRIBERS"
+	print channel1.subscribers
+	print "DONE"
+	user2.subscriptions.append(channel2)
+	user2.subscriptions.append(channel1)
+
+	print "SUBSCRIPTIONS OF USER 2"
+	print user2.subscriptions
+	print "DONE"
+	db.session.add(user1)
+	db.session.add(user2)
+	db.session.add(channel1)
+	db.session.add(channel2)
+	db.session.commit()
+
+	app.run(debug = True, use_reloader=False)
