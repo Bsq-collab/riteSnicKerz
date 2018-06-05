e216630311090fdf764cdf3e510b5aef844e65a7
bleung329 <bleung@stuy.edu>
2018-06-05 12:45:12 -0400

    thank u wise garden gnome

--- janus/testingRoom/ORMTest/studentORMtest.py
+++ janus/testingRoom/ORMTest/studentORMtest.py
@@ -12,8 +12,9 @@ db = SQLAlchemy(app)
 #classes.query.all()
 
 #association table
+
 studentclass = db.Table('ssa_table',
-	db.Column('section',db.Integer,db.ForeignKey('sections')),
+	db.Column('section',db.Integer,db.ForeignKey('sections.section_id')),
 	db.Column('student_osis',db.Integer,db.ForeignKey('students.osis'))
 )
 
@@ -22,16 +23,38 @@ class students(db.Model):
 	osis = db.Column(db.Integer())
 	#classSections - will give you list of class sections that student is in. Need to go up one more in order to access class itself.
 
-#Uhh.... new plan? make a classes db and sections db and then relate the two.
 
 class sections(db.Model):
 	id = db.Column('sectionID',db.Integer,primary_key=True)
 	section_id = db.Column(db.Integer())
-	roster = db.relationship('roster',secondary=studentclass,backref=db.backref('classSections'))
+	class_code = db.Column(db.String(10),db.ForeignKey("classes.class_code"))
+	roster = db.relationship('students',secondary=studentclass,backref=db.backref('classSections'))
 
 
 class classes(db.Model):
 	id = db.Column('classID',db.Integer,primary_key=True)
-	sections = db.relationship("sections",backref="class")
+	sections = db.relationship("sections",backref="upperClass",lazy = True)
 	max_students = db.Column(db.Integer())
+	class_code = db.Column(db.String(10))
 
+	def __init__(self,classCode):
+		self.class_code = classCode
+
+if __name__ == '__main__':
+	db.create_all()
+	sect1=sections(section_id=3)
+	class1 = classes("MKS")
+	class1.sections.append(sect1)
+	student1=students(osis = 1111)
+	student2=students(osis=10101)
+	sect1.roster.append(student1)
+	sect1.roster.append(student2)
+	print "Sections "+str(class1.sections)
+	print "Class code: "+str(class1.class_code)
+	print "Class of sect1: "+str(sect1.upperClass.class_code)
+	print "Roster of sect1 "+str(sect1.roster)
+	print "ID of student 0 of sect1 "+str(sect1.roster[0].osis)
+	print "Sections of student: " +str(student1.classSections)
+	print "Class of Section of student " +str(student1.classSections[0].upperClass.class_code)
+	#db.commit()
+	app.run(debug = True, use_reloader=False)
