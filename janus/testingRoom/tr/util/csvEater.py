7e46b94c2ab24c12101a3a9b0948d54e798e5310
bleung329 <bleung@stuy.edu>
2018-06-12 19:06:17 -0400

    eeeeeeee

--- janus/testingRoom/tr/util/csvEater.py
+++ janus/testingRoom/tr/util/csvEater.py
@@ -1,13 +1,20 @@
 import csv
 
-fin = "../data/Class-List1.csv"
-fout = "../data/Class-ListOut.csv"  
-allcopy = []
-with open(fout) as csvout:
-    with open(fin) as csvin:
-        reader = csv.reader(csvin)
-        writer = csv.writer(csvout)
-        for row in reader:
-            allcopy.append(row)
-            
-print allcopy
+
+def csvEater():
+	with open("NewClassList.csv") as csvfile:
+		reader = csv.reader(csvfile)
+		prevClass = ''
+		for row in reader:
+			if row[0] == 'Course Code':
+				pass
+			else:
+				if prevClass != row[0]:
+					db.session.add(newClass)
+					newClass = classes(row[0], row[2])
+					prevClass = row[0]
+				else:
+					sectionHolder[row[1]] = {"teacher":row[3],"room":'',"roster":[]}
+		db.session.add(newClass)
+		db.session.commit()
+
